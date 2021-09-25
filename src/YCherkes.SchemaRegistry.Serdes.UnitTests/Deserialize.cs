79d71aafa53f9a1242efb21e4d05f64cfb2f237c
ycherkes <smurtdev@gmail.com>
2021-09-25 16:36:21 +0300

    + Unit tests

---
+++ src/YCherkes.SchemaRegistry.Serdes.UnitTests/Deserialize.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.Linq;
+using Avro.Specific;
+using Confluent.Kafka;
+using Confluent.Kafka.Examples.AvroSpecific;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Moq;
+using Xunit;
+using YCherkes.SchemaRegistry.Serdes.Avro;
+
+namespace YCherkes.SchemaRegistry.Serdes.UnitTests
+{
+    public class Deserialize
+    {
+        private readonly ISchemaRegistryClient _schemaRegistryClient;
+        private readonly Dictionary<string, int> _store = new();
+        private readonly string _testTopic;
+
+        public Deserialize()
+        {
+            _testTopic = "topic";
+            var schemaRegistryMock = new Mock<ISchemaRegistryClient>();
+            schemaRegistryMock.Setup(x => x.ConstructValueSubjectName(_testTopic, It.IsAny<string>())).Returns($"{_testTopic}-value");
+            schemaRegistryMock.Setup(x => x.RegisterSchemaAsync("topic-value", It.IsAny<string>())).ReturnsAsync(
+                (string topic, string schema) => _store.TryGetValue(schema, out int id) ? id : _store[schema] = _store.Count + 1
+            );
+            schemaRegistryMock.Setup(x => x.GetSchemaAsync(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(
+                (int id, string format) => new Schema(_store.First(x => x.Value == id).Key, null, SchemaType.Avro)
+            );
+            _schemaRegistryClient = schemaRegistryMock.Object;
+        }
+
+        [Fact]
+        public void ISpecificRecord_MultiSchemaDeserializer()
+        {
+            var serializer = new AvroSerializer<ISpecificRecord>(_schemaRegistryClient);
+            var deserializer = new MultiSchemaAvroDeserializer(_schemaRegistryClient);
+
+            var user = new User
+            {
+                favorite_color = "blue",
+                favorite_number = 100,
+                name = "awesome"
+            };
+
+            var bytes = serializer.SerializeAsync(user, new SerializationContext(MessageComponentType.Value, _testTopic)).Result;
+            var resultUser = deserializer.DeserializeAsync(bytes, false, new SerializationContext(MessageComponentType.Value, _testTopic)).Result as User;
+
+            Assert.NotNull(resultUser);
+            Assert.Equal(user.name, resultUser.name);
+            Assert.Equal(user.favorite_color, resultUser.favorite_color);
+            Assert.Equal(user.favorite_number, resultUser.favorite_number);
+        }
+
+        [Fact]
+        public void Multiple_ISpecificRecords_MultiSchemaDeserializer()
+        {
+            var serializer = new AvroSerializer<ISpecificRecord>(_schemaRegistryClient);
+            var deserializer = new MultiSchemaAvroDeserializer(_schemaRegistryClient);
+
+            var user = new User
+            {
+                favorite_color = "blue",
+                favorite_number = 100,
+                name = "awesome"
+            };
+
+            var car = new Car
+            {
+                color = "blue",
+                name = "great_brand"
+            };
+
+            var bytesUser = serializer.SerializeAsync(user, new SerializationContext(MessageComponentType.Value, _testTopic)).Result;
+            var resultUser = deserializer.DeserializeAsync(bytesUser, false, new SerializationContext(MessageComponentType.Value, _testTopic)).Result as User;
+
+            Assert.NotNull(resultUser);
+            Assert.Equal(user.name, resultUser.name);
+            Assert.Equal(user.favorite_color, resultUser.favorite_color);
+            Assert.Equal(user.favorite_number, resultUser.favorite_number);
+
+            var bytesCar = serializer.SerializeAsync(car, new SerializationContext(MessageComponentType.Value, _testTopic)).Result;
+            var resultCar = deserializer.DeserializeAsync(bytesCar, false, new SerializationContext(MessageComponentType.Value, _testTopic)).Result as Car;
+
+            Assert.NotNull(resultCar);
+            Assert.Equal(car.name, resultCar.name);
+            Assert.Equal(car.color, resultCar.color);
+        }
+    }
+}
